8f9146de4188f2dd861215ecccc03913b76e11f7
Jantosik <jakub.jantosik@accenture.com>
2017-01-31 15:49:48 +0100

    3rd commit, who the fuck cares about commit messages anyway

--- src/mesh.js
+++ src/mesh.js
@@ -23,9 +23,10 @@ export default class Mesh {
                 }
             }
         }
+        return result;
     }
 
-    create({ positions, indices, normals, uvs }) {
+    create({ positions, indices, normals, uvs, sizes }) {
         if (this.vao !== null) {
             return;
         }
@@ -33,9 +34,6 @@ export default class Mesh {
         const gl = this.gl;
         this._vao = gl.createVertexArray();
         this._buffer = gl.createBuffer();
-
-        gl.bindVertexArray(vertexArray);
-        
 
         let f = [];
         if (typeof positions !== 'undefined') {
@@ -47,7 +45,25 @@ export default class Mesh {
         if (typeof uvs !== 'undefined') {
             f.push(uvs);
         }
-        f = flatten(f);
+        f = new Float32Array(flatten(f));
+
+        gl.bindVertexArray(this.vao);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, f, gl.STATIC_DRAW);
+
+        if (typeof positions !== 'undefined') {
+            gl.enableVertexAttribArray(POSITION_LOCATION);
+            gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0);
+        }
+        if (typeof normals !== 'undefined') {
+        }
+        if (typeof uvs !== 'undefined') {
+        }
+
+        gl.bindBuffer(null);
+
+        gl.bindVertexArray(null);
 
     }
 
